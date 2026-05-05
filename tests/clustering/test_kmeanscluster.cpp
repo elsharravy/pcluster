688a103e3769cc720548555a0107a32f16792de9
ElSharravy <jakub15000@gmail.com>
2026-05-05 09:28:07 +0200

    Tests: Update kmeanscluster test according to changes in KMenasCluster class

--- tests/clustering/test_kmeanscluster.cpp
+++ tests/clustering/test_kmeanscluster.cpp
@@ -9,12 +9,16 @@ TEST(KMeansClusterTest, addingLocation) {
 
 	KMeansCluster cluster(Location( 20,30));
 
-	cluster.addLocationUpdateCenter(Location(40, 50));
-	cluster.addLocationUpdateCenter(Location(15, 25));
-	cluster.addLocationUpdateCenter(Location(12.5, 55));
-	cluster.addLocationUpdateCenter(Location(45, 20));
-	cluster.addLocationUpdateCenter(Location(25, 40));
-	cluster.addLocationUpdateCenter(Location(12.5, 10));
+	cluster.addLocation(Location(20, 30));
+	cluster.addLocation(Location(40, 50));
+	cluster.addLocation(Location(15, 25));
+	cluster.addLocation(Location(12.5, 55));
+	cluster.addLocation(Location(45, 20));
+	cluster.addLocation(Location(25, 40));
+	cluster.addLocation(Location(12.5, 10));
+	
+	cluster.recalculateCenter();
+	cluster.clear();
 
 	float latitude = cluster.getCenter().getLatitude();
 	float longitude = cluster.getCenter().getLongitude();
