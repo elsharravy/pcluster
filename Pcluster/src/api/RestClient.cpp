9c812d36a9267afa50f3198910d4883d6184edbe
ElSharravy <jakub15000@gmail.com>
2026-05-06 15:36:26 +0200

    Refactor: Error handling inside RestClient

--- Pcluster/src/api/RestClient.cpp
+++ Pcluster/src/api/RestClient.cpp
@@ -2,10 +2,16 @@
 
 #include <cpr/api.h>
 
-std::pair<std::string, long > RestClient::get(std::string_view url, int page, int per_page)
+std::pair<std::string, long > pcluster::RestClient::get(std::string_view url, int page, int per_page)
 {
     cpr::Response r = cpr::Get(cpr::Url{ url },
-        cpr::Authentication{ "user", "pass", cpr::AuthMode::BASIC },
-        cpr::Parameters{ {"page", std::to_string(page) }, {"per_page", std::to_string(page) }});
-    return std::make_pair<>(r.text, r.status_code);
+        cpr::Parameters{ {"page", std::to_string(page) }, {"per_page", std::to_string(per_page) }});
+
+    if (r.error.code != cpr::ErrorCode::OK)
+    {
+        throw std::runtime_error(
+            "HTTP transport error: " + r.error.message);
+    }
+
+    return {r.text, r.status_code};
 }
--- Pcluster/src/api/RestClient.h
+++ Pcluster/src/api/RestClient.h
@@ -4,7 +4,12 @@
 #include <string_view>
 #include <utility>
 
-class RestClient {
-public:
-	static std::pair<std::string, long > get(std::string_view url, int page, int per_page);
- };
+namespace pcluster {
+
+	class RestClient {
+	public:
+		static std::pair<std::string, long > get(std::string_view url, int page, int per_page);
+	};
+
+}
+
